868c21cde72d9723535a5026b6a6ff1558fae092
Daniel Doblado <danieldoblado@gmail.com>
2020-06-13 12:25:42 +0200

    Test read file

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
+const fs = require('fs');
 const core = require('@actions/core');
 const github = require('@actions/github');
+
+const {validateChangelog} = require('./validate');
+
+const ingnoreActionMessage = `-Changelog` // ToDo: make it customizable
 
 try {
   const CHANGELOGS = JSON.parse(core.getInput('changelogs'))
 
   // Not do anything if -Changelog is a commit message
-  const ignoreAction = github.context.payload.commits.some(commit => commit.message === '-Action')
-  console.log({ignoreAction})
+  const ignoreAction = github.context.payload.commits.some(commit => commit.message === ingnoreActionMessage)
   if (ignoreAction) {
-    console.log('Exit')
+    console.log(`Exit the action due to message with ${ingnoreActionMessage}`)
     process.exit(0)
   }
 
   CHANGELOGS.forEach(changelog => {
-    console.log(changelog)
+    // For each watchFolder check if it has modified files
+      // If it has, verify the changelog
+      // If is not modified warn it
+
+    const changelogContent = fr.readFileSync(changelog.file, { encoding: 'utf-8' });
+    console.log(changelogContent);
+    validateChangelog(changelogContent);
+  
   })
   
   const context = JSON.stringify(github.context, undefined, 2)
