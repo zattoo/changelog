b20fad0e5e021fe25de74237d08c196806b51272
Daniel Doblado <danieldoblado@gmail.com>
2020-06-14 20:40:56 +0200

    Tidy up

--- src/index.js
+++ src/index.js
@@ -1,50 +1,14 @@
 const fs = require('fs');
 const core = require('@actions/core');
-const {context, getOctokit} = require('@actions/github');
-// const { graphql } = require("@octokit/graphql");
+const {context} = require('@actions/github');
 
 const {validateChangelog} = require('./validate');
 const ingnoreActionMessage = `-Changelog` // ToDo: make it customizable
 
 const repo = context.payload.repository;
 const owner = repo.owner;
-const args = { owner: owner.name || owner.login, repo: repo.name };
-
-
-// const getFiles = async (commits) => {
-//   let modifiedFiles = [];
-
-//   await Promise.all(commits.forEach(async commit => {
-//     args.ref = commit.id || commit.sha;
-
-//     console.log('Calling gh.repos.getCommit() with args', args)
-
-//     const res = await gh.repos.getCommit(args);
-//     if (res.data && res.data.files) {
-//       res.data.files.forEach(file => {
-//         modifiedFiles.push(file.filename)
-//       })
-//     }
-//   }));
-
-//   core.get
-
-//   return modifiedFiles;
-// }
 
 try {
-  // const gh = getOctokit(core.getInput('token'));
-
-  // Exclude merge commits
-  let commits = context.payload.commits.filter(c => ! c.parents || 1 === c.parents.length)
-  if ('push' === context.eventName) {
-		commits = commits.filter(c => c.distinct);
-  }
-
-  // getFiles(commits)
-  const files = core.getInput('modifiedFiles')
-  console.log(files)
-
   const CHANGELOGS = JSON.parse(core.getInput('changelogs'))
 
   // Not do anything if -Changelog is a commit message
@@ -55,7 +19,7 @@ try {
   }
 
   CHANGELOGS.forEach(changelog => {
-    // For each watchFolder check if it has modified files
+    // TODO: For each watchFolder check if it has modified files
       // If it has, verify the changelog
       // If is not modified warn it
 
