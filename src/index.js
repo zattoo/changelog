16ee81121fbd119aba5941ade8379eb62ac727ac
Daniel Doblado <danieldoblado@gmail.com>
2020-06-14 23:21:05 +0200

    Good cleanup

--- src/index.js
+++ src/index.js
@@ -1,57 +1,44 @@
 const fs = require('fs');
 const core = require('@actions/core');
-const github = require('@actions/github');
+const { context, getOctokit } = require('@actions/github');
 
 const { validateChangelog } = require('./validate');
+const { getModifiedFiles } = require('./files');
 
-const ingnoreActionMessage = '-Changelog'; // ToDo: make it customizable
+const run = async () => {
+  try {
+    const token = core.getInput('token', { required: true });
+    const changelogs = JSON.parse(core.getInput('changelogs', { required: true }));
+    const ingnoreActionMessage = core.getInput('ingnoreActionMessage');
+    const octokit = getOctokit(token);
 
-const { context } = github;
-const repo = context.payload.repository.name;
-const owner = context.payload.repository.owner.name;
-const { sha } = context;
+    // Not do anything if -Changelog is a commit message
+    const ignoreAction = context.payload.commits
+      .some((commit) => commit.message === ingnoreActionMessage);
+    if (ignoreAction) {
+      console.log(`Exit the action due to message with ${ingnoreActionMessage}`);
+      process.exit(0);
+    }
 
-console.log(repo, owner, sha);
+    const repo = context.payload.repository.name;
+    const owner = context.payload.repository.owner.name;
+    const { sha } = context;
 
-const getPR = async (octokit) => {
-  const PRS = await octokit.repos.listPullRequestsAssociatedWithCommit({
-    owner,
-    repo,
-    sha,
-  });
+    const files = await getModifiedFiles(octokit, repo, owner, sha);
+    console.log(files);
 
-  console.log(PRS);
+    changelogs.forEach((changelog) => {
+      // TODO: For each watchFolder check if it has modified files
+      // If it has, verify the changelog
+      // If is not modified warn it
+      const changelogContent = fs.readFileSync(changelog.file, { encoding: 'utf-8' });
+      validateChangelog(changelogContent);
+    });
+
+    console.log(`The event context: ${JSON.stringify(context, undefined, 2)}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 };
 
-try {
-  const CHANGELOGS = JSON.parse(core.getInput('changelogs'));
-  // const modifiedFiles = JSON.parse(core.getInput('modifiedFiles'));
-
-  const token = core.getInput('token', { required: true });
-  console.log(token);
-
-  const octokit = github.getOctokit(token);
-
-  getPR(octokit);
-
-  // Not do anything if -Changelog is a commit message
-  const ignoreAction = context.payload.commits
-    .some((commit) => commit.message === ingnoreActionMessage);
-  if (ignoreAction) {
-    console.log(`Exit the action due to message with ${ingnoreActionMessage}`);
-    process.exit(0);
-  }
-
-  CHANGELOGS.forEach((changelog) => {
-    // TODO: For each watchFolder check if it has modified files
-    // If it has, verify the changelog
-    // If is not modified warn it
-
-    const changelogContent = fs.readFileSync(changelog.file, { encoding: 'utf-8' });
-    validateChangelog(changelogContent);
-  });
-
-  console.log(`The event context: ${JSON.stringify(context, undefined, 2)}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+run();
--- src/validate.js
+++ src/validate.js
@@ -1,42 +1,42 @@
 const changeTypes = [
-    'Added',
-    'Changed',
-    'Deprecated',
-    'Removed',
-    'Fixed',
-    'Security',
-    'Infrastructure', // Custom
-    'Updated', // Custom
+  'Added',
+  'Changed',
+  'Deprecated',
+  'Removed',
+  'Fixed',
+  'Security',
+  'Infrastructure', // Custom
+  'Updated', // Custom
 ];
 
 /**
- * Check if there is only an unique title
- * @param {string} text 
- */
-const validateH1 = (text) => {
-    const titles = text.match(/^#(\s*\w*)$/gm) || []
-    
-    if (titles.length === 0) {
-        throw(new Error('No title is present'))
-    } else if (titles.length > 1) {
-        throw(new Error('Only one title is allowed'))
-    } else if (!checkHeadingSpaces(titles.slice(0, 1), 1)) {
-        throw(new Error('Title has more than one space'))
-    }
-}
-
-/**
- * Validades if the given text heading 
+ * Validades if the given text heading
  * has invalid spaces
  * @param {string} text
  * @param {number} level
  * @returns {boolean}
  */
 const checkHeadingSpaces = (text, level) => {
-    const re = `^${'#'.repeat(level)}\\s([^\\s].*)$`
-    const regex = new RegExp(re, 'g');
-    return regex.test(text)
-}
+  const re = `^${'#'.repeat(level)}\\s([^\\s].*)$`;
+  const regex = new RegExp(re, 'g');
+  return regex.test(text);
+};
+
+/**
+ * Check if there is only an unique title
+ * @param {string} text
+ */
+const validateH1 = (text) => {
+  const titles = text.match(/^#(\s*\w*)$/gm) || [];
+
+  if (titles.length === 0) {
+    throw (new Error('No title is present'));
+  } else if (titles.length > 1) {
+    throw (new Error('Only one title is allowed'));
+  } else if (!checkHeadingSpaces(titles.slice(0, 1), 1)) {
+    throw (new Error('Title has more than one space'));
+  }
+};
 
 /**
  * If the semver string a is greater than b, return 1.
@@ -47,81 +47,80 @@ const checkHeadingSpaces = (text, level) => {
  * @see https://github.com/substack/semver-compare
  */
 const compareSemVer = (a, b) => {
-    const pa = a.split('.');
-    const pb = b.split('.');
-    for (var i = 0; i < 3; i++) {
-        var na = Number(pa[i]);
-        var nb = Number(pb[i]);
-        if (na > nb) return 1;
-        if (nb > na) return -1;
-        if (!isNaN(na) && isNaN(nb)) return 1;
-        if (isNaN(na) && !isNaN(nb)) return -1;
-    }
-}
-
+  const pa = a.split('.');
+  const pb = b.split('.');
+  for (let i = 0; i < 3; i++) {
+    const na = Number(pa[i]);
+    const nb = Number(pb[i]);
+    if (na > nb) return 1;
+    if (nb > na) return -1;
+    if (!isNaN(na) && isNaN(nb)) return 1;
+    if (isNaN(na) && !isNaN(nb)) return -1;
+  }
+};
 
 /**
  * Validate H3 headings
- * @param {string} text 
+ * @param {string} text
  */
 const validateH3 = (text) => {
-    const headings = text.match(/^###(\s*\w*)$/gm) || []
+  const headings = text.match(/^###(\s*\w*)$/gm) || [];
 
-    headings.forEach(heading => {
-        const re = `^###\\s(${changeTypes.join('|')})$`
-        const regex = new RegExp(re, 'g');
+  headings.forEach((heading) => {
+    const re = `^###\\s(${changeTypes.join('|')})$`;
+    const regex = new RegExp(re, 'g');
 
-        if (!regex.test(heading)) {
-            throw(new Error(`${heading} is not a valid change type`))
-        } else if (!checkHeadingSpaces(heading, 3)) {
-            throw(new Error(`${heading} has incorrect spaces`))
-        }
-    })
-}
+    if (!regex.test(heading)) {
+      throw (new Error(`${heading} is not a valid change type`));
+    } else if (!checkHeadingSpaces(heading, 3)) {
+      throw (new Error(`${heading} has incorrect spaces`));
+    }
+  });
+};
 
 const validateH2 = (text) => {
-    const headings = text.match(/^##\s.*$/gm) || []
-    
-    if (headings.filter(heading => heading.toLowerCase().includes('unreleased')).length > 1) {
-        throw (new Error('Only one unreleased heading is allowed'));
+  const headings = text.match(/^##\s.*$/gm) || [];
+
+  if (headings.filter((heading) => heading.toLowerCase().includes('unreleased')).length > 1) {
+    throw (new Error('Only one unreleased heading is allowed'));
+  }
+
+  let previousVersion;
+  headings.forEach((heading) => {
+    const [, currentVersion] = heading.match(/^##\s\[(\d+.\d+.\d+)\]/) || [];
+
+    if (
+      !/^##\s\[*Unreleased\]*$/gm.test(heading)
+            && !/^##\s\[\d+.\d+.\d+\] - (\d+.\d+.\d+|\[*Unreleased\]*)$/gm.test(text)
+    ) {
+      throw (new Error(`${heading} is not valid`));
+    } else if (!checkHeadingSpaces(heading, 2)) {
+      throw (new Error(`${heading} has incorrect spaces`));
     }
-    
-    let previousVersion
-    headings.forEach(heading => {
-        const [, currentVersion] = heading.match(/^##\s\[(\d+.\d+.\d+)\]/) || []
 
-        if (
-            !/^##\s\[*Unreleased\]*$/gm.test(heading) &&
-            !/^##\s\[\d+.\d+.\d+\] - (\d+.\d+.\d+|\[*Unreleased\]*)$/gm.test(text)
-        ) {
-            throw (new Error(`${heading} is not valid`))
-        } else if (!checkHeadingSpaces(heading, 2)) {
-            throw(new Error(`${heading} has incorrect spaces`))
-        }
+    if (previousVersion && currentVersion) {
+      const compare = compareSemVer(previousVersion, currentVersion);
 
-        if (previousVersion && currentVersion) {
-            const compare = compareSemVer(previousVersion, currentVersion)
+      if (previousVersion === currentVersion) {
+        throw (new Error(`Version ${previousVersion} can't be the same as a previous version ${currentVersion}`));
+      } else if (compare === -1) {
+        throw (new Error(`Version ${previousVersion} can't be smaller than a previous version ${currentVersion}`));
+      }
+    }
 
-            if (previousVersion === currentVersion) {
-                throw(new Error(`Version ${previousVersion} can't be the same as a previous version ${currentVersion}`))
-            } else if (compare === -1) {
-                throw(new Error(`Version ${previousVersion} can't be smaller than a previous version ${currentVersion}`))
-            } 
-        }
-        
-        if (currentVersion) {
-            previousVersion = currentVersion
-        }
-    })
-}
+    if (currentVersion) {
+      previousVersion = currentVersion;
+    }
+  });
+};
 
 const validateChangelog = (text) => {
-    validateH1(text);
-    validateH2(text);
-    validateH3(text);
-    return true
-}
+  validateH1(text);
+  validateH2(text);
+  validateH3(text);
+  return true;
+};
 
 module.exports = {
-    validateChangelog
-}
+  validateChangelog,
+};
