c9e9b2e4e620c1edfc770a502db0bc3c5cbd2cde
Daniel Doblado <danieldoblado@gmail.com>
2020-06-29 15:00:34 +0200

    Test release

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const { getModifiedFiles } = require('./files');
 const run = async () => {
   try {
     const token = core.getInput('token', { required: true });
-    const changelogs = JSON.parse(core.getInput('changelogs', { required: true }));
+    const files = JSON.parse(core.getInput('files', { required: true }));
     const ingnoreActionMessage = core.getInput('ignoreActionMessage');
     const octokit = getOctokit(token);
 
@@ -16,6 +16,9 @@ const run = async () => {
     const owner = context.payload.repository.full_name.split('/')[0];
     const pullNumber = context.payload.pull_request.number;
     const labels = context.payload.pull_request.labels.map((label) => label.name);
+    const branch = context.payload.pull_request.head.ref;
+
+    console.log({ branch });
 
     // Ignore the action if -Changelog label (or custom name) exists
     if (labels.includes(ingnoreActionMessage)) {
@@ -25,12 +28,18 @@ const run = async () => {
 
     const modifiedFiles = await getModifiedFiles(octokit, repo, owner, pullNumber);
 
-    changelogs.forEach((changelog) => {
+    files.forEach((changelog) => {
       // Check if at least one file was modified in the watchFolder
       if (modifiedFiles.some((filename) => filename.startsWith(changelog.watchFolder))) {
         // Check if changelog is in the modified files
-        if (!modifiedFiles.includes(changelog.file)) {
+        if (!modifiedFiles.includes(changelog.changelog)) {
           core.setFailed(`Files in ${changelog.watchFolder} have been modified but ${changelog.file} was not modified`);
+        }
+
+        // If the branch is release check if it has a package.json with a version the same as the fist H3
+        // and a valid date
+        if (branch === 'release') {
+          console.log('BRANCH IS RELEASE');
         }
       }
 
