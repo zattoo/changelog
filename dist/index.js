ebb31d049d79a2d5c14165d45e4d239fdaa0256f
Daniel Doblado <danieldoblado@gmail.com>
2021-10-27 17:22:55 +0200

    Add error logic for delimiter

--- dist/index.js
+++ dist/index.js
@@ -9653,7 +9653,7 @@ const changeTypes = [
  *
  * @see https://regex101.com/r/v5VmTx/2
  */
-const reH2 = /^##\s\[?(Unreleased)\]?|^##\s\[((?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-(?:(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?:[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)\] - (Unreleased|(?:\d\d?\d?\d?[-/.]\d\d?[-/.]\d\d?\d?\d))$/;
+const reH2 = /^##\s\[?(Unreleased)\]?|^##\s\[((?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-(?:(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?:[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)\]\s*\W\s*(Unreleased|(?:\d\d?\d?\d?[-/.]\d\d?[-/.]\d\d?\d?\d))$/;
 
 /**
  * Validates if the given text heading
@@ -9780,6 +9780,13 @@ const validateChangelog = (text) => {
             }
         } else if (version) {
             const [, unreleased, versionValue, date] = line.match(reH2) || [];
+
+            if (!line.includes(' - ')) {
+                errors.push({
+                    message: 'Use a valid dash separation " - " between the version and date',
+                    lines: [lineNumber],
+                });
+            }
 
             if (!isUnreleased(unreleased, date)) {
                 if (!date) {
